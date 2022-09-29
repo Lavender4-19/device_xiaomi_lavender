f6acfea9f46250427853e458b60319a386e361bf
OmarAlCoptan <omarcoptan9@gmail.com>
2022-09-30 01:50:09 +0200

    lavender: EvolutionX Bringup
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- evolution_lavender.mk
+++ evolution_lavender.mk
@@ -8,8 +8,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common EvolutionX stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -17,10 +20,8 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lavender
+PRODUCT_NAME := evolution_lavender
 PRODUCT_MODEL := Redmi Note 7
-
-DEVICE_MAINTAINER := Ratoriku, Hazama25
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
