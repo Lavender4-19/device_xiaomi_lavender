f54bf91668561caca92f79a0234df07979a6b8b6
Fbriiiii <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-10-16 16:47:38 +0700

    Rice13-4.19 Bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,9 +4,9 @@ rm -rf hardware/qcom-caf/sdm660/media
 rm -rf hardware/qcom-caf/sdm660/audio
 
 # dt
-git clone --depth=1 https://github.com/OmarAlCoptan/device_xiaomi_sdm660-common.git -b 13-4.19 device/xiaomi/sdm660-common
-git clone --depth=1 https://github.com/OmarAlCoptan/vendor_xiaomi_lavender.git -b 13-4.19 vendor/xiaomi/lavender
-git clone --depth=1 https://github.com/OmarAlCoptan/vendor_xiaomi_sdm660-common.git -b 13-4.19 vendor/xiaomi/sdm660-common
+git clone --depth=1 https://github.com/Lavender4.19/device_xiaomi_sdm660-common.git -b 13-4.19 device/xiaomi/sdm660-common
+git clone --depth=1 https://github.com/Lavender4-19/vendor_xiaomi_lavender.git -b rice13-4.19 vendor/xiaomi/lavender
+git clone --depth=1 https://github.com/Lavender4.19/vendor_xiaomi_sdm660-common.git -b 13-4.19 vendor/xiaomi/sdm660-common
 git clone --depth=1 https://github.com/pix106/android_kernel_xiaomi_southwest-4.19.git -b main kernel/xiaomi/sdm660
 
 # hals
